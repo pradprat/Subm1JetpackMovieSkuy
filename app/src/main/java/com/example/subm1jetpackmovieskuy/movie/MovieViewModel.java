0fbe04e790b1e9e690f33a63becff1fbcb6239d4
pradika pratama <33866097+pradprat@users.noreply.github.com>
2019-10-11 11:07:06 +0700

    first commit

---
+++ app/src/main/java/com/example/subm1jetpackmovieskuy/movie/MovieViewModel.java
@@ -0,0 +1,7 @@
+package com.example.subm1jetpackmovieskuy.movie;
+
+import androidx.lifecycle.ViewModel;
+
+public class MovieViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/subm1jetpackmovieskuy/tvShow/TvShowViewModel.java
@@ -0,0 +1,7 @@
+package com.example.subm1jetpackmovieskuy.tvShow;
+
+import androidx.lifecycle.ViewModel;
+
+public class TvShowViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
