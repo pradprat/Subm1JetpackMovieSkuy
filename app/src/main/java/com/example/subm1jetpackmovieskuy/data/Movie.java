d7a4616ce0e3b8e0c38bf5b8cad2aca3280331d9
pradika pratama <33866097+pradprat@users.noreply.github.com>
2019-10-14 14:47:45 +0700

    add data

--- app/src/main/java/com/example/subm1jetpackmovieskuy/data/Movie.java
+++ app/src/main/java/com/example/subm1jetpackmovieskuy/data/Movie.java
@@ -1,33 +1,19 @@
 package com.example.subm1jetpackmovieskuy.data;
 
+import android.media.Image;
+
 public class Movie {
     public Double popularity;
     public Integer voteCount;
     public Boolean video;
-    public String posterPath;
+    public int posterPath;
     public Integer id;
     public Boolean adult;
-    public String backdropPath;
+    public int backdropPath;
     public String originalLanguage;
     public String originalTitle;
     public String title;
     public Double voteAverage;
     public String overview;
     public String releaseDate;
-
-    public Movie(double popularity, Integer voteCount, Boolean video, String posterPath, Integer id, boolean adult, String backdropPath, String originalLanguage, String originalTitle, String title, double voteAverage, String overview, String releaseDate) {
-        this.popularity = popularity;
-        this.voteCount = voteCount;
-        this.video = video;
-        this.posterPath = posterPath;
-        this.id = id;
-        this.adult = adult;
-        this.backdropPath = backdropPath;
-        this.originalLanguage = originalLanguage;
-        this.originalTitle = originalTitle;
-        this.title = title;
-        this.voteAverage = voteAverage;
-        this.overview = overview;
-        this.releaseDate = releaseDate;
-    }
 }
